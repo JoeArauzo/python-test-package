2c301cee9a11479e592f14594cda231e6445dafa
Joe Arauzo <joe@arauzo.net>
2019-06-03 12:23:57 -0700

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='testpackage',
+        version='0.0.1',
+        description='My test package from github repo',
+        url='git@github.com:JoeArauzo/python-test-package.git',
+        author='Joe Arauzo',
+        author_email='joe@arauzo.net',
+        license='unlicense',
+        packages='testpackage',
+        zip_safe=False
+)
